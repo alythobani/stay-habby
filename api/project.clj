b552b1fcdc6fc026171b3ee61e3a0d33321124cb
Aly Thobani <athobani9@gmail.com>
2018-07-25 10:53:23 -0700

    Increase repl timeout to 120 seconds

--- api/project.clj
+++ api/project.clj
@@ -14,4 +14,8 @@
                  [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot api.core
   :target-path "target/%s"
+  :repl-options {;; If nREPL takes too long to load it may timeout,
+                 ;; increase this to wait longer before timing out.
+                 ;; Defaults to 30000 (30 seconds)
+                 :timeout 120000}
   :profiles {:uberjar {:aot :all}})

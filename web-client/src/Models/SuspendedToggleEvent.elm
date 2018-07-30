b0ab7effde85179126345b93c03ef6eb5df1a55a
Aly Thobani <athobani9@gmail.com>
2018-07-29 17:16:58 -0700

    Tiny removal of unused imports

--- web-client/src/Models/SuspendedToggleEvent.elm
+++ web-client/src/Models/SuspendedToggleEvent.elm
@@ -1,7 +1,7 @@
 module Models.SuspendedToggleEvent exposing (..)
 
 import Json.Decode as Decode
-import Json.Decode.Pipeline exposing (decode, hardcoded, optional, required)
+import Json.Decode.Pipeline exposing (decode, required)
 import Models.YmdDate exposing (YmdDate, decodeYmdDate)
 
 

3b45caf9330e84f59d6c0f8b12d3aa419796e242
Aly Thobani <athobani9@gmail.com>
2019-01-18 15:17:19 -0800

    Show current goal type on Edit Goal form

--- web-client/src/View.elm
+++ web-client/src/View.elm
@@ -854,6 +854,37 @@ renderSetHabitDataShortcut showSetHabitDataShortcut setHabitDataShortcutHabitNam
 
 renderEditGoalDialog : Bool -> Maybe Habit.Habit -> Html Msg
 renderEditGoalDialog showEditGoalDialog habit =
+    let
+        currentGoal : Maybe Habit.FrequencyChangeRecord
+        currentGoal =
+            case habit of
+                Just h ->
+                    case h of
+                        Habit.GoodHabit gh ->
+                            List.head <| List.reverse gh.targetFrequencies
+
+                        Habit.BadHabit bh ->
+                            List.head <| List.reverse bh.thresholdFrequencies
+
+                Nothing ->
+                    Nothing
+
+        currentGoalTag =
+            case currentGoal of
+                Just fcr ->
+                    case fcr.newFrequency of
+                        Habit.EveryXDayFrequency _ ->
+                            "Y Per X Days"
+
+                        Habit.TotalWeekFrequency _ ->
+                            "X Per Week"
+
+                        Habit.SpecificDayOfWeekFrequency _ ->
+                            "Specific Days of Week"
+
+                Nothing ->
+                    ""
+    in
     div
         [ classList
             [ ( "edit-goal-dialog", True )
@@ -869,9 +900,13 @@ renderEditGoalDialog showEditGoalDialog habit =
             [ class "edit-goal-dialog-form" ]
             [ div
                 [ class "edit-goal-dialog-form-header" ]
-                [ text <| "Habit" <? .name <|| Habit.getCommonFields <|| habit ]
+                [ text <| "Error: habit not found" <? .name <|| Habit.getCommonFields <|| habit ]
+            , div [ class "edit-goal-dialog-form-line-break" ] []
             , div
-                [ class "edit-goal-dialog-form-current-goal" ]
-                []
+                [ class "edit-goal-dialog-form-current-goal-text" ]
+                [ text "Current Goal" ]
+            , div
+                [ class "edit-goal-dialog-form-current-goal-tag" ]
+                [ button [] [ text currentGoalTag ] ]
             ]
         ]

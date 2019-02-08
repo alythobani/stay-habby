3d20cbf6e33115e5f1a0cd6c07db98058fdcb8bc
Aly Thobani <athobani9@gmail.com>
2019-02-08 09:30:34 -0800

    Fix migrations 1 and 2 to use UTC time and only consider `total_week_frequency` goals to be weekly goals

--- api/migrations/1-goal-frequencies-array.migration.js
+++ api/migrations/1-goal-frequencies-array.migration.js
@@ -2,14 +2,19 @@
 // an array `target_frequencies` for good habits or `threshold_frequencies` for bad habits
 
 db.habits.find().snapshot().forEach(habit => {
+  let frequencyChangeDate = habit._id.getTimestamp();
+  frequencyChangeDate.setUTCHours(0);
+  frequencyChangeDate.setUTCMinutes(0);
+  frequencyChangeDate.setUTCSeconds(0);
+  frequencyChangeDate.setUTCMilliseconds(0);
   if (habit.type_name === "good_habit") {
     habit.target_frequencies = [{
-      frequency_change_date: habit._id.getTimestamp(),
+      frequency_change_date: frequencyChangeDate,
       new_frequency: habit.target_frequency
     }];
   } else {
     habit.threshold_frequencies = [{
-      frequency_change_date: habit._id.getTimestamp(),
+      frequency_change_date: frequencyChangeDate,
       new_frequency: habit.threshold_frequency
     }];
   }
--- api/migrations/2-frequency-start-date-end-date.migration.js
+++ api/migrations/2-frequency-start-date-end-date.migration.js
@@ -1,18 +1,18 @@
 /// 2nd migration file - rename `frequency_change_date` of `frequency_change_record`s to
 // `start_date` and add a null `end_date` as well (OK since currently habits only have one goal each).
-// Also: for weekly goals (`total_week_frequency` or `specific_day_of_week_frequency`), ensure the
-// `start_date` lies on a Monday, else set it to the first Monday after the `frequency_change_date`.
+// Also: for weekly goals (`total_week_frequency`), ensure the `start_date` lies on a Monday, else
+// set it to the first Monday after the `frequency_change_date`.
 
 function addDaysToDate(date, numDays) {
   let newDate = new Date(date);
-  newDate.setDate(newDate.getDate() + numDays);
+  newDate.setUTCDate(newDate.getUTCDate() + numDays);
   return newDate;
 }
 
 function getFirstMondayAfterDate(date) {
   // Note that days in Javascript are numbered 0 for Sunday, ..., 6 for Saturday.
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
-  let numDaysToAdd = (8 - date.getDay()) % 7;  // 1 for Sunday, 0 for Monday, 6 for Tuesday, ..., 2 for Saturday
+  let numDaysToAdd = (8 - date.getUTCDay()) % 7;  // 1 for Sunday, 0 for Monday, 6 for Tuesday, ..., 2 for Saturday
   return addDaysToDate(date, numDaysToAdd);
 }
 
@@ -28,8 +28,8 @@ db.habits.find().snapshot().forEach(habit => {
   let frequencyChangeRecord = getFirstFrequencyChangeRecord(habit);
   let frequency = frequencyChangeRecord.new_frequency;
   let new_start_date = frequencyChangeRecord.frequency_change_date;
-  if (frequency.type_name !== "every_x_days_frequency") {
-    // the goal is a weekly goal (specific_day_of_week_frequency or total_week_frequency)
+  if (frequency.type_name === "total_week_frequency") {
+    // the goal is a weekly goal
     new_start_date = getFirstMondayAfterDate(new_start_date);
   }
   let new_frequencies = [{

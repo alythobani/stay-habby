ae5e2cf21c57d664f0f812c46fea81f1c3d785af
Aly Thobani <athobani9@gmail.com>
2024-01-30 16:03:44 -0800

    Local dev setup (#27)
    * Add DEV_INSTRUCTIONS markdown file copied from earlier history of README.md
    
    * Prettier ran on webpack config js file
    
    * Point client graphQL API endpoint to localhost instead of AWS server URL
    
    * Add data-backups/ and vim backup files to gitignore
    
    ---------
    
    Co-authored-by: Aly Thobani <alythobani@Alys-MacBook-Pro.local>

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -1,26 +1,28 @@
-var path = require('path');
-var webpack = require('webpack');
-var merge = require('webpack-merge');
-var autoprefixer = require('autoprefixer');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var merge = require("webpack-merge");
+var autoprefixer = require("autoprefixer");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
+const LOCAL_GRAPHQL_URL = "http://localhost:8888/graphql";
+const AWS_GRAPHQL_URL = "https://stayhabbyserver.xyz/graphql";
 
-const prod = 'production';
-const dev = 'development';
+const prod = "production";
+const dev = "development";
 
 // determine build env
-const TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? prod : dev;
+const TARGET_ENV = process.env.npm_lifecycle_event === "build" ? prod : dev;
 const isDev = TARGET_ENV == dev;
 const isProd = TARGET_ENV == prod;
 
 // entry and output path/filename variables
-const entryPath = path.join(__dirname, 'src/index.js');
-const outputPath = path.join(__dirname, 'dist');
-const outputFilename = isProd ? '[name]-[hash].js' : '[name].js'
+const entryPath = path.join(__dirname, "src/index.js");
+const outputPath = path.join(__dirname, "dist");
+const outputFilename = isProd ? "[name]-[hash].js" : "[name].js";
 
-console.log('WEBPACK GO! Building for ' + TARGET_ENV);
+console.log("WEBPACK GO! Building for " + TARGET_ENV);
 
 // common webpack config (valid for dev and prod)
 var commonConfig = {
@@ -29,79 +31,84 @@ var commonConfig = {
     filename: `${outputFilename}`,
   },
   resolve: {
-    extensions: ['.js', '.elm'],
-    modules: ['node_modules']
+    extensions: [".js", ".elm"],
+    modules: ["node_modules"],
   },
   module: {
     noParse: /\.elm$/,
-    rules: [{
+    rules: [
+      {
         test: /\.(eot|ttf|woff|woff2|svg)$/,
-        use: 'file-loader?publicPath=../../&name=[hash].[ext]'
+        use: "file-loader?publicPath=../../&name=[hash].[ext]",
       },
       {
         test: /\index.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['es2015']
-          }
-        }
-      }
-    ]
+            presets: ["es2015"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.LoaderOptionsPlugin({
       options: {
-        postcss: [autoprefixer()]
-      }
+        postcss: [autoprefixer()],
+      },
     }),
     new webpack.DefinePlugin({
-      __WEBPACK_CONSTANT_API_BASE_URL__: JSON.stringify("https://stayhabbyserver.xyz/graphql")
+      __WEBPACK_CONSTANT_API_BASE_URL__: JSON.stringify(LOCAL_GRAPHQL_URL),
     }),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body',
-      filename: 'index.html'
-    })
-  ]
-}
+      template: "src/index.html",
+      inject: "body",
+      filename: "index.html",
+    }),
+  ],
+};
 
 // additional webpack settings for local env (when invoked by 'npm start')
 if (isDev === true) {
   module.exports = merge(commonConfig, {
-    entry: [
-      'webpack-dev-server/client?http://localhost:8080',
-      entryPath
-    ],
+    entry: ["webpack-dev-server/client?http://localhost:8080", entryPath],
     devServer: {
       // serve index.html in place of 404 responses
       historyApiFallback: true,
-      contentBase: './dist',
-      hot: true
+      contentBase: "./dist",
+      hot: true,
     },
     module: {
-      rules: [{
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        use: [{
-          loader: 'elm-webpack-loader',
-          options: {
-            verbose: true,
-            debug: false
-          }
-        }]
-      }, {
-        test: /\.sc?ss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-      }]
+      rules: [
+        {
+          test: /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          use: [
+            {
+              loader: "elm-webpack-loader",
+              options: {
+                verbose: true,
+                debug: false,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.sc?ss$/,
+          use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        },
+      ],
     },
     plugins: [
-      new CopyWebpackPlugin([{
-        from: 'assets/',
-        to: 'assets/'
-      }])
-    ]
+      new CopyWebpackPlugin([
+        {
+          from: "assets/",
+          to: "assets/",
+        },
+      ]),
+    ],
   });
 }
 
@@ -110,37 +117,42 @@ if (isProd === true) {
   module.exports = merge(commonConfig, {
     entry: entryPath,
     module: {
-      rules: [{
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        use: 'elm-webpack-loader'
-      }, {
-        test: /\.sc?ss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
-        })
-      }]
+      rules: [
+        {
+          test: /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          use: "elm-webpack-loader",
+        },
+        {
+          test: /\.sc?ss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "postcss-loader", "sass-loader"],
+          }),
+        },
+      ],
     },
     plugins: [
       new ExtractTextPlugin({
-        filename: '[name]-[hash].css',
+        filename: "[name]-[hash].css",
         allChunks: true,
       }),
-      new CopyWebpackPlugin([{
-        from: 'assets/',
-        to: 'assets/'
-      }]),
+      new CopyWebpackPlugin([
+        {
+          from: "assets/",
+          to: "assets/",
+        },
+      ]),
 
       // extract CSS into a separate file
       // minify & mangle JS/CSS
       new webpack.optimize.UglifyJsPlugin({
         minimize: true,
         compressor: {
-          warnings: false
-        }
+          warnings: false,
+        },
         // mangle:  true
-      })
-    ]
+      }),
+    ],
   });
 }
